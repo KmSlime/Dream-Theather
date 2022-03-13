0b6c64c0f35beb23a8cc95521b950e0381585cdb
Duc-Hoang <hoanghuuduc777@gmail.com>
2022-03-14 03:51:24 +0700

    change package main activity

--- app/src/main/java/com/Dream/AppDatVeXemPhim/screens/activity/MainActivity.java
+++ app/src/main/java/com/Dream/AppDatVeXemPhim/screens/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.Dream.AppDatVeXemPhim;
+package com.Dream.AppDatVeXemPhim.screens.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.Dream.AppDatVeXemPhim.R;
 
 public class MainActivity extends AppCompatActivity {
 
