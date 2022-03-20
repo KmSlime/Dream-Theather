adda38d0910ae6aa7f4a683a4121d944321f93cd
Slime <guenvesley@gmail.com>
2022-03-21 02:53:04 +0700

    đã xong login bằng google

--- app/src/main/java/com/dream/dreamtheather/Login.java
+++ app/src/main/java/com/dream/dreamtheather/Login.java
@@ -1,14 +1,114 @@
 package com.dream.dreamtheather;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    //ánh xạ
+    ImageButton btnEyeShow;
+    EditText edtPassword;
+    Button LoginEmail;
+
+    //
+    private GoogleApiClient googleApiClient;
+    private static final int SIGN_IN = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnEyeShow = findViewById(R.id.btnEyeShow);
+        edtPassword = findViewById(R.id.edtPassword);
+        LoginEmail = findViewById(R.id.LoginEmail);
+
+        btnEyeShow.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        edtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        edtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions).build();
+
+        LoginEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN);
+            }
+        });
+    }
+
+    public void btnRegister(View view) {
+
+    }
+
+    public void ResetPass(View view) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()) {
+                startActivity(new Intent(Login.this, UserProfile.class)); // sau này sẽ sửa dòng này
+                finish();
+            } else Toast.makeText(this, "Đăng nhập với Google thất bại!", Toast.LENGTH_LONG).show();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
